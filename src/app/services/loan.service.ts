431dcc96b384ee42273a3f6e8ca7ec1d68e76231
donovanbrown <browncdonovan@gmail.com>
2020-11-01 19:15:54 -0500

    pwa-test

--- src/app/services/loan.service.ts
+++ src/app/services/loan.service.ts
@@ -14,12 +14,6 @@ export class LoanService {
   stateClear = this.stateSource.asObservable();
 
   constructor() {
-    // this.logs = [
-    //   {id: '1', text: 'Generated components', date: new Date('12/26/2017 12:54:23')},
-    //   {id: '2', text: 'Added Bootstrap', date: new Date('12/27/2017 9:33:13')},
-    //   {id: '3', text: 'Added logs component', date: new Date('12/27/2017 12:00:23')}
-    // ]
-
     this.logs = [];
   }
 
